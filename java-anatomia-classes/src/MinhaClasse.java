15376b26e6118e1180985d75c692b09e6e57e5f6
juupm <julianapmodesto@gmail.com>
2023-05-29 19:06:16 +0200

    basic-java-anatomy

---
+++ java-anatomia-classes/src/MinhaClasse.java
@@ -0,0 +1,14 @@
+public class MinhaClasse {
+    public static void main(String[] args) {
+        String primeiroNome = "Juliana";
+        String segundoNome = "Modesto";
+
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+
+        System.out.println(nomeCompleto);
+        }
+
+    public static String nomeCompleto (String primeiroNome, String segundoNome) {
+        return "Resultado do metodo " + primeiroNome.concat(" ").concat(segundoNome);
+    }
+}
